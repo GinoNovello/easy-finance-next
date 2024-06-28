2da6894b545f2693e998475b83c84b01d4b555b1
GinoNovello <ginonovello9@gmail.com>
2024-06-28 03:06:44 -0300

    chore: Add UserAuthForm component and update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
+import { UserAuthForm } from "@/components/ui/user-auth-form";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      Futuro login
-      <Link
-        className="border rounded-lg bg-green-300 text-rose-500 p-2"
-        href="/dashboard"
-      >
-        Dashboard
-      </Link>
+    <div className="flex items-center justify-center min-h-dvh">
+      <UserAuthForm />
     </div>
   );
 }
